c51d182aeabc96ef49cb118fb7a1385432670940
Sterling Tarng <starng3@yahoo.com>
2015-05-25 16:57:49 -0700

    Added zoom and pan to ImageViews

--- app/src/main/java/com/seniorproject/ibeaconnavigation/FloorplanNavigationActivity.java
+++ app/src/main/java/com/seniorproject/ibeaconnavigation/FloorplanNavigationActivity.java
@@ -2,9 +2,12 @@ package com.seniorproject.ibeaconnavigation;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.matabii.dev.scaleimageview.ScaleImageView;
 
 /**
  * Created by Calvin on 5/2/2015.
