4e438593e69f97b80f64f14c5d9ed8527477197e
calvwng <calvwng@gmail.com>
2015-05-30 01:33:52 -0700

    Floorplan nav now triggered by beacon detection
    * Changed bg service to a regular Service instead of IntentService (so I
    can control when it stops itself, after a beacon is detected)
    
    * In this revision, service starts when "Navigate to Room" button in
    MapActivity is pressed, but I'll probably update this to immediately
    start the service when the target room is selected, and only start the
    Google Nav intent if the beacon is not found within a certain amount of
    time or tries.
    
    * Also made map marker in MapActivity's map fragment show details by
    default

--- app/src/main/java/com/seniorproject/ibeaconnavigation/BuildingProximityService.java
+++ app/src/main/java/com/seniorproject/ibeaconnavigation/BuildingProximityService.java
@@ -1,53 +1,79 @@
 package com.seniorproject.ibeaconnavigation;
 
-import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
 
 /**
  * Created by Calvin on 5/2/2015.
  */
-public class BuildingProximityService extends IntentService {
+public class BuildingProximityService extends Service implements BeaconConsumer {
+    protected static final String TAG = "BuildingProximityService";
+    private BeaconManager beaconManager;
 
-    public BuildingProximityService() {
-        super("BuildingProximityService");
-    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.bind(this);
+        // Supposedly this is the RadBeacon Layout
+        beaconManager
+                .getBeaconParsers()
+                .add(new BeaconParser()
+                        .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
     }
 
     @Override
-    protected void onHandleIntent(Intent workIntent) {
-        // Get data from intent and check current proximity of target building or its beacons
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-        Toast.makeText(this, "Simulating proximity check (10 sec)...", Toast.LENGTH_SHORT).show();
-        Log.d("BuildProxService", "Simulating proximity check...");
-        SystemClock.sleep(10000); // Simulated wait time
-        Log.d("BuildProxService", "Done simulating proximity check.");
 
-        Toast.makeText(this, "Simulating successful proximity check...", Toast.LENGTH_SHORT).show();
-        Intent fpNavIntent = new Intent(getBaseContext(), FloorplanNavigationActivity.class);
-        fpNavIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        getApplication().startActivity(fpNavIntent);
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(final Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+//                    for (Beacon beacon : beacons) {
+//                        Log.d("cwong", "The beacon " + beacon.getBluetoothAddress() +
+//                                " is about " + beacon.getDistance() + " meters away.");
+//                    }
+//                    Log.d("cwong", "Number of beacons: " + beacons.size());
+
+                    try {
+                        BeaconManager.getInstanceForApplication(getApplicationContext())
+                                .stopRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+                    }
+                    catch (RemoteException re) {
+                        Log.d("bpServ", "" + re);
+                    }
+
+                    // Finding the beacon triggers the floorplan navigation and stops this service
+                    Toast.makeText(BuildingProximityService.this, "Found the room beacon!", Toast.LENGTH_SHORT).show();
+                    Intent fpNavIntent = new Intent(getBaseContext(), FloorplanNavigationActivity.class);
+                    fpNavIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    getApplication().startActivity(fpNavIntent);
+                    stopSelf();
+                }
+            }
+        });
 
         try {
-            BeaconManager.getInstanceForApplication(getApplicationContext())
-                    .stopRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        }
-        catch (RemoteException re) {
-            Log.d("bpServ", "" + re);
-        }
-
-        // Service automatically stops upon completion
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
     }
 }
--- app/src/main/java/com/seniorproject/ibeaconnavigation/MapActivity.java
+++ app/src/main/java/com/seniorproject/ibeaconnavigation/MapActivity.java
@@ -1,33 +1,15 @@
 package com.seniorproject.ibeaconnavigation;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 import android.app.Activity;
 import android.content.Intent;
-import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.util.Calendar;
 
 public class MapActivity extends Activity implements OnMapReadyCallback {
-//, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
-//    // FIXME: Unused b/c may use beacon detection vs. location checking to trigger indoor nav
-//    private GoogleApiClient mGoogleApiClient;
-//    private Location mLastLocation;
-//    private double mLatitude;
-//    private double mLongitude;
-//    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +25,7 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         navButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Simulate launching floorplan navigation logic when close to building
+                // Launch background service to detect iBeacon
                 Intent bpService = new Intent(MapActivity.this, BuildingProximityService.class);
                 MapActivity.this.startService(bpService);
 
@@ -57,58 +39,7 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
                 MapActivity.this.startActivity(mapIntent);
             }
         });
-
-//        // FIXME: Unused b/c may use beacon detection vs. location checking to trigger indoor nav
-//        // Build & connect to Google API Client to ask for last known location
-//        buildGoogleApiClient();
-//        mGoogleApiClient.connect();
-//
-//        mWebView = (WebView)findViewById(R.id.map_webView);
-//        mWebView.setWebViewClient(new WebViewClient());
-//        mWebView.getSettings().setJavaScriptEnabled(true);
     }
-
-    //    // FIXME: Unused b/c may use beacon detection vs. location checking to trigger indoor nav
-//    @Override
-//    public void finish() {
-//        mGoogleApiClient.disconnect();
-//        super.finish();
-//    }
-//
-//    protected synchronized void buildGoogleApiClient() {
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .addConnectionCallbacks(this)        // Required callback interface
-//                .addOnConnectionFailedListener(this) // Required callback interface
-//                .addApi(LocationServices.API)
-//                .build();
-//    }
-//
-//    @Override
-//    public void onConnected(Bundle connectionHint) {
-//        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-//                mGoogleApiClient);
-//        if (mLastLocation != null) {
-//            mLatitude = mLastLocation.getLatitude();
-//            mLongitude = mLastLocation.getLongitude();
-////            Toast.makeText(this, "Lat/Long : " + mLatitude + ", " + mLongitude, Toast.LENGTH_SHORT).show();
-//            String lastKnownLocation = mLatitude + "," + mLongitude;
-//            mWebView.loadUrl("http://maps.google.com/maps?saddr=" + lastKnownLocation
-//                    + "&daddr=Frank+E.+Pilling,+San+Luis+Obispo,+CA");
-//        }
-//    }
-//
-//    @Override
-//    public void onConnectionSuspended(int i) {
-//        // The connection has been interrupted.
-//        // Disable any UI components that depend on Google APIs
-//        // until onConnected() is called.
-//        Toast.makeText(this, "Connection to Google API Client suspended.", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    @Override
-//    public void onConnectionFailed(ConnectionResult connectionResult) {
-//        Toast.makeText(this, "Connection to Google API Client failed.", Toast.LENGTH_SHORT).show();
-//    }
 
 
     @Override
@@ -118,10 +49,11 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         map.setMyLocationEnabled(true);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(targetCoord, 13));
 
-        map.addMarker(new MarkerOptions()
-                .title("Frank E. Pilling Computer Science")
-                .snippet("A hub of hands-on computational creativity and analysis.")
-                .position(targetCoord));
+        Marker marker = map.addMarker(new MarkerOptions()
+            .title("Frank E. Pilling Computer Science")
+            .snippet("A hub of hands-on computational creativity and analysis.")
+            .position(targetCoord));
+        marker.showInfoWindow();
     }
 }
 
